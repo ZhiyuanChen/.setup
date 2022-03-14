b8872bfc3fd51501d7f9d73802c852768114f148
Zhiyuan Chen <this@zyc.ai>
2022-03-14 16:47:12 +0800

    add vsc installation
    Signed-off-by: Zhiyuan Chen <this@zyc.ai>

--- setup.sh
+++ setup.sh
@@ -29,3 +29,9 @@ curl -L git.io/antigen > /usr/share/zsh/antigen.zsh
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 cp .zshrc ~/
 
+# setup vsc
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt install code-insiders
+
+
