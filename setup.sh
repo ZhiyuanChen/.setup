205339db1aadb55b4c2c393cf51d1ce08e4906d6
Zhiyuan Chen <this@zyc.ai>
2022-03-15 07:38:36 +0800

    switch gpu driver to graphics-drivers/ppa
    Signed-off-by: Zhiyuan Chen <this@zyc.ai>

--- setup.sh
+++ setup.sh
@@ -9,6 +9,8 @@ sudo apt install flatpak -y
 flatpak install https://dl.flathub.org/repo/appstream/com.github.joseexposito.touche.flatpakref -y
 
 # setup GPU
+sudo add-apt-repository ppa:graphics-drivers/ppa -y
+sudo apt update
 sudo apt install nvidia-driver-510 -y
 
 # setup pinyin
@@ -19,11 +21,12 @@ sudo apt install fcitx-ui-qimpanel -y
 sudo apt remove cmdtest yarn -y
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update
-sudo apt-get install yarn -y
+sudo apt update
+sudo apt install yarn -y
 
 # setup vim
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt update
 sudo apt install neovim -y
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
 
