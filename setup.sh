0628d43c26b74413cc60de23455a424a1979ce88
Zhiyuan Chen <this@zyc.ai>
2022-03-15 09:47:38 +0800

    fix antigen is not properly installed

--- setup.sh
+++ setup.sh
@@ -48,7 +48,8 @@ sudo apt update
 sudo apt install code-insiders
 
 # setup Oh My Zsh
-sudo curl -L git.io/antigen > /usr/share/zsh/antigen.zsh
+curl -L git.io/antigen > antigen.zsh
+sudo mv antigen.zsh /usr/share/zsh/antigen.zsh
 sudo sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 cp .zshrc ~/
 
