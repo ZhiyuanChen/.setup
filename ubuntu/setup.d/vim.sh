64f67548dc4037299dd9e082ac92ab01eb0e623f
Zhiyuan Chen <this@zyc.ai>
2022-08-30 02:28:08 +0800

    use lts nodjs for better support

--- ubuntu/setup.d/vim.sh
+++ ubuntu/setup.d/vim.sh
@@ -1,13 +1,12 @@
-# setup yarn
-sudo apt remove cmdtest yarn -y
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update
-sudo apt install cargo yarn -y
+# setup cargo (rust) and yarn (nodejs)
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs cargo
+sudo npm install yarn -y
+
+
 
 # setup vim
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
 sudo apt install neovim -y
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
-
