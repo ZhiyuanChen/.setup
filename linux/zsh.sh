0c3f7d9af7459ad16e865063091bd1f25967f521
Zhiyuan Chen <chenzhiyuan@pjlab.org.cn>
2024-02-23 02:47:51 +0000

    auto append exec zsh to .bashrc
    Signed-off-by: Zhiyuan Chen <chenzhiyuan@pjlab.org.cn>

--- linux/zsh.sh
+++ linux/zsh.sh
@@ -2,3 +2,5 @@
 curl -L git.io/antigen-nightly > /usr/share/zsh/antigen.zsh
 cp "$(dirname "${BASH_SOURCE[0]}")/.zshrc" ~/
 chsh -s $(which zsh)
+echo -e "if [ -f /usr/bin/zsh ]; then\n    exec /usr/bin/zsh\nfi" >> ~/.bashrc
+
