8779db1606abcabdddcd1fb0f4a18be9bff84091
Zhiyuan Chen <chenzhiyuan@pjlab.org.cn>
2024-02-23 03:35:16 +0000

    add rebound
    Signed-off-by: Zhiyuan Chen <chenzhiyuan@pjlab.org.cn>

--- ubuntu/utils.sh
+++ ubuntu/utils.sh
@@ -3,4 +3,4 @@ apt install -y tmux zsh  # shell
 apt install -y curl git wget  # web
 apt install -y btop htop  # monitor
 apt install -y ranger fzf  # file
-apt install -y silversearcher-ag tldr newsboat # other
+apt install -y silversearcher-ag tldr newsboat rebound-cli # other
