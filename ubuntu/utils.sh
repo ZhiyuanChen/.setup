926bd93c5fa2af370f4c50ea728d1f6a35468e09
Zhiyuan Chen <chenzhiyuan@pjlab.org.cn>
2024-02-23 09:13:59 +0000

    install openssh-server
    Signed-off-by: Zhiyuan Chen <chenzhiyuan@pjlab.org.cn>

--- ubuntu/utils.sh
+++ ubuntu/utils.sh
@@ -1,6 +1,7 @@
 apt update
 apt install -y tmux zsh  # shell
-apt install -y curl git wget  # web
+apt install -y curl git wget openssh-server  # conncetion
+service ssh start
 apt install -y btop htop  # monitor
 apt install -y ranger fzf  # file
-apt install -y silversearcher-ag tldr newsboat rebound-cli thefuck # other
+apt install -y silversearcher-ag tldr newsboat thefuck # other
